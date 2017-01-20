603f8a2ac187c5fa25c214bcfa49940670ef2c53
Pierre Kestener <pierre.kestener@cea.fr>
2017-01-20 17:24:50 +0100

    and a Mandelbrot functor with multi-dimensionnal range execution policy

---
+++ code/exercises/mandelbrot_kokkos/kokkos_mdrange/README.txt
@@ -0,0 +1,5 @@
+Just edit :
+- main.cpp
+- mandelbrot.h
+
+fill the blanks marked with /* TODO */
---
+++ code/exercises/mandelbrot_kokkos/kokkos_mdrange/main.cpp
@@ -0,0 +1,147 @@
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+#include <omp.h>
+
+#include "constants.h"
+#include "kokkos_shared.h"
+#include "mandelbrot.h"
+
+#ifdef CUDA
+#include "CudaTimer.h"
+#else // OpenMP
+#include "OpenMPTimer.h"
+#endif
+
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+  /*
+   * Initialize kokkos (host + device)
+   */
+#ifdef CUDA
+  // Initialize Host mirror device
+  Kokkos::HostSpace::execution_space::initialize(1);
+  //const unsigned device_count = Kokkos::Cuda::detect_device_count();
+
+  // Use the first device:
+  Kokkos::Cuda::initialize( Kokkos::Cuda::SelectDevice(0) );
+#else // OpenMP CPU
+  Kokkos::initialize(argc, argv);
+#endif
+
+  {
+    std::cout << "##########################\n";
+    std::cout << "KOKKOS CONFIG             \n";
+    std::cout << "##########################\n";
+    
+    std::ostringstream msg;
+    std::cout << "Kokkos configuration" << std::endl;
+    if ( Kokkos::hwloc::available() ) {
+      msg << "hwloc( NUMA[" << Kokkos::hwloc::get_available_numa_count()
+          << "] x CORE["    << Kokkos::hwloc::get_available_cores_per_numa()
+          << "] x HT["      << Kokkos::hwloc::get_available_threads_per_core()
+          << "] )"
+          << std::endl ;
+    }
+#if defined( CUDA )
+    Kokkos::Cuda::print_configuration( msg );
+#else
+    Kokkos::OpenMP::print_configuration( msg );
+#endif
+    std::cout << msg.str();
+    std::cout << "##########################\n";
+  }
+
+#ifdef CUDA
+  CudaTimer timer;
+#else
+  OpenMPTimer timer;
+#endif
+
+  Constants constants = Constants();
+
+  // Allocate data array for Mandelbrot set computation
+  /* TODO 1 : allocate data buffer image and imagehost (host mirror of data)*/
+  /* DataArray     image     = ... */
+  /* DataArrayHost imageHost = ... */
+
+  /*
+   * Actual computation (GPU with CUDA or CPU with OpenMP)
+   */
+  timer.start();
+
+  {
+    //using namespace Kokkos::Experimental;
+    MandelbrotFunctor functor(image, constants);
+    using range2d_t =
+      Kokkos::Experimental::MDRangePolicy< Kokkos::Experimental::Rank<2> ,
+					   Kokkos::IndexType<int> >;
+    /* TODO 2 : create a multi-dimensional range policy variable named "range" */
+    Kokkos::Experimental::md_parallel_for(range, functor);
+  }
+  
+  printf("end of loop reached ...\n");
+
+  timer.stop();
+  printf("Time: %lf seconds.\n", timer.elapsed());
+
+  // copy back results from device to host
+  /* TODO 3 : deep copy image to imageHost */
+  
+  // print aesthetically, dont read this part
+  int xmax=80;
+  int ymax=60;
+  for(int y=0;y<ymax;y++) {
+    printf("\n");
+    for(int x=0;x<xmax;x++) {
+
+      int val = imageHost(y*constants.HEIGHT/ymax,x*constants.WIDTH/xmax);
+      
+      if (val==200) printf("&");
+      else if (val==42) printf("X");
+      else if(val>64) printf("#");
+      else if(val>32) printf(":");
+      else if(val>8) printf(".");
+      else printf(" ");
+    }
+  }
+  
+  printf("\n");
+
+  // save color ppm file
+  if (0) {
+    FILE* myfile = fopen("mandelbrot.ppm","w");
+    
+    fprintf(myfile, "P6 %d %d 255\n", constants.WIDTH , constants.HEIGHT);
+    for(unsigned int i=0; i<constants.WIDTH; ++i) {
+      for(unsigned int j=0; j<constants.HEIGHT; ++j) {
+	
+	unsigned char data;
+	// create an arbitrary RBG code mapping values taken by imageHost
+	data = imageHost(i,j) % 4 * 64;
+	fwrite(&data,1,1,myfile);
+	data = imageHost(i,j) % 8 * 32;
+	fwrite(&data,1,1,myfile);
+	data = imageHost(i,j) % 16 * 16;
+	fwrite(&data,1,1,myfile);
+      }
+    }
+
+    fclose(myfile);
+  }
+  
+   
+#ifdef CUDA
+  Kokkos::Cuda::finalize();
+  Kokkos::HostSpace::execution_space::finalize();
+#else
+  Kokkos::finalize();
+#endif
+
+  return 0;
+}
