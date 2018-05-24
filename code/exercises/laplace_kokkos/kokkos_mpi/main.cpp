a5c6f612c81b796fd2a42c95fcedf85b1d90fdf5
Pierre Kestener <pierre.kestener@cea.fr>
2018-05-24 16:40:38 +0200

    update kokkos laplace mpi

--- code/exercises/laplace_kokkos/kokkos_mpi/main.cpp
+++ code/exercises/laplace_kokkos/kokkos_mpi/main.cpp
@@ -9,81 +9,33 @@
 #include "DataContextKokkos.h"
 
 #include "OpenMPTimer.h"
+#ifdef KOKKOS_ENABLE_CUDA
 #include "CudaTimer.h"
+#endif // KOKKOS_ENABLE_CUDA
 
 #include "laplace2d_serial_kernel.h"
 #include "laplace2d_kokkos_kernel.h"
 
 #include "check_results.h"
 
-#ifdef CUDA
+#ifdef KOKKOS_ENABLE_CUDA
 #include <cuda.h>
-#endif // CUDA
+#endif // KOKKOS_ENABLE_CUDA
 
 #include <mpi.h>
 
-int main(int argc, char* argv[])
+// ========================================================================
+// ========================================================================
+void test_laplace(int NX, int NY)
 {
 
-  /* Init MPI */
-  MPI_Init(&argc, &argv);
-
-  /*
-   * Initialize kokkos
-   */
-  // this regular initialization: let Kokkos use hwloc to decide
-  // - how many OpenMP threads ? Default is to use all available core on node
-  // - how GPU and MPI tasks are mapped
-  Kokkos::initialize(argc, argv);
-// #ifdef CUDA
-//   // regular init
-//   Kokkos::initialize(argc, argv);
-// #else // OpenMP
-//   // for OpenMP we might want to take control here
-//   Kokkos::OpenMP::Initialize
-// #endif
-    
   int rank, nRanks;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &nRanks);
-  
-  if (rank==0) {
-    std::cout << "##########################\n";
-    std::cout << "KOKKOS CONFIG             \n";
-    std::cout << "##########################\n";
-    
-    std::ostringstream msg;
-    std::cout << "Kokkos configuration" << std::endl;
-    if ( Kokkos::hwloc::available() ) {
-      msg << "hwloc( NUMA[" << Kokkos::hwloc::get_available_numa_count()
-          << "] x CORE["    << Kokkos::hwloc::get_available_cores_per_numa()
-          << "] x HT["      << Kokkos::hwloc::get_available_threads_per_core()
-          << "] )"
-          << std::endl ;
-    }
-#if defined( CUDA )
-    Kokkos::Cuda::print_configuration( msg );
-#else
-    Kokkos::OpenMP::print_configuration( msg );
-#endif
-    std::cout << msg.str();
-    std::cout << "##########################\n";
-  }
 
-#ifdef CUDA
-  /*
-   * Just checking, which MPI rank mapped with which GPU ?
-   */
-  int cudaDeviceId;
-  cudaGetDevice(&cudaDeviceId);
-  std::cout << "I'm MPI task #" << rank << " pinned to GPU #" << cudaDeviceId << "\n";
-#endif
-  
   /*
    * Inititalize some parameters.
    */
-  int NX = 1024;
-  int NY = 1024;
   int iter_max = 1000;
 
 #ifdef USE_DOUBLE
@@ -170,7 +122,7 @@ int main(int argc, char* argv[])
       context_kokkos.rhs(index) = expr(-10.0*(x*x + y*y));
     });
 
-#ifdef CUDA
+#ifdef KOKKOS_ENABLE_CUDA
   CudaTimer timer2;
 #else
   OpenMPTimer timer2;
@@ -186,6 +138,64 @@ int main(int argc, char* argv[])
     printf("serial %dx%d: %8.4f secondes\n",NX,NY,runtime_serial);
     printf("kokkos %dx%d: %8.4f secondes\n",NX,NY,runtime_kokkos);
   }
+
+} // test_laplace
+
+// ========================================================================
+// ========================================================================
+int main(int argc, char* argv[])
+{
+
+  /* Init MPI */
+  MPI_Init(&argc, &argv);
+
+  /*
+   * Initialize kokkos
+   */
+  // this regular initialization: let Kokkos use hwloc to decide
+  // - how many OpenMP threads ? Default is to use all available core on node
+  // - how GPU and MPI tasks are mapped
+  Kokkos::initialize(argc, argv);
+// #ifdef CUDA
+//   // regular init
+//   Kokkos::initialize(argc, argv);
+// #else // OpenMP
+//   // for OpenMP we might want to take control here
+//   Kokkos::OpenMP::Initialize
+// #endif
+    
+  int rank, nRanks;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nRanks);
+  
+  if (rank==0) {
+    std::cout << "##########################\n";
+    std::cout << "KOKKOS CONFIG             \n";
+    std::cout << "##########################\n";
+    
+    std::ostringstream msg;
+    std::cout << "Kokkos configuration" << std::endl;
+    if ( Kokkos::hwloc::available() ) {
+      msg << "hwloc( NUMA[" << Kokkos::hwloc::get_available_numa_count()
+          << "] x CORE["    << Kokkos::hwloc::get_available_cores_per_numa()
+          << "] x HT["      << Kokkos::hwloc::get_available_threads_per_core()
+          << "] )"
+          << std::endl ;
+    }
+    Kokkos::print_configuration( msg );
+    std::cout << msg.str();
+    std::cout << "##########################\n";
+  }
+
+#ifdef KOKKOS_ENABLE_CUDA
+  /*
+   * Just checking, which MPI rank mapped with which GPU ?
+   */
+  int cudaDeviceId;
+  cudaGetDevice(&cudaDeviceId);
+  std::cout << "I'm MPI task #" << rank << " pinned to GPU #" << cudaDeviceId << "\n";
+#endif // KOKKOS_ENABLE_CUDA
+  
   
   Kokkos::finalize();
 
