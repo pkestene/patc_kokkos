838e07b051500e3ae72869a653a2b7a0baca89b5
Pierre Kestener <pierre.kestener@cea.fr>
2018-05-25 15:22:15 +0200

    submit gpu ouessant

---
+++ doc/ouessant/submit_job_gpu1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -x
+#BSUB -gpu "num=1:mode=exclusive_process:mps=no:j_exclusive=yes"
+#BSUB -J test_device_query
+#BSUB -o test_device_query.%J.out
+#BSUB -n 1
+#BSUB -q computet1
+#BSUB -W 00:05
+
+module load at/10.0 cuda/9.0
+
+echo "########################################################"
+./deviceQuery
+echo "########################################################"
+env | grep CUDA
+
