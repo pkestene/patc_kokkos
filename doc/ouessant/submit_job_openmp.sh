644815abee07e08b5144d7c97da578c7d291b253
Pierre Kestener <pierre.kestener@cea.fr>
2018-05-31 23:03:56 +0200

    update submit job ouessant openmp

--- doc/ouessant/submit_job_openmp.sh
+++ doc/ouessant/submit_job_openmp.sh
@@ -4,12 +4,11 @@
 #BSUB -n 1
 #BSUB -o test_saxpy_omp.%J.out
 #BSUB -q computet1
-#BSUB -R "affinity[core(1):cpubind=core:distribute=pack]"
 #BSUB -a "p8aff(20,8,8,pack)"
-#BSUB -R "span[ptile=10]"
+#BSUB -R "span[ptile=1]"
 #BSUB -W 00:05
 
-# module load gcc/4.8/ompi/2.1
+module load at/10.0
 # module load cuda/9.0
 
 # here we ask
