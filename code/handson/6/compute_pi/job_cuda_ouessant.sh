708b8efd5c385b1d863654427e060c833f561986
Pierre Kestener <pierre.kestener@cea.fr>
2019-03-14 18:47:45 +0100

    update compute_pi - add submit script

---
+++ code/handson/6/compute_pi/job_cuda_ouessant.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -x
+#BSUB -n 1
+# reserve 1 GPU per node
+#BSUB -gpu "num=1:mode=exclusive_process:mps=no:j_exclusive=yes"
+#BSUB -J stencil_cuda
+#BSUB -o stencil_cuda.%J.log
+#BSUB -q computet1
+#BSUB -a p8aff(1,8,1,balance)
+# 1 tasks per nodes 
+#BSUB -R "span[ptile=1]"
+#BSUB -W 01:00
+
+# launch this script with bsub < job_cuda_ouessant.sh
+
+./compute_pi.cuda
